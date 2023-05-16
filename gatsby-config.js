b74697c88db6213e8bc7405b4ee77eaed39c1d20
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-05-16 13:26:35 +0200

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
       options: {
